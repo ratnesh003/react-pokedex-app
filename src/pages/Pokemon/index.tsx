1efd9d42caeaa562b1c40caa0095504e2a749e97
Ratnesh Pasi <ratneshpasi03@gmail.com>
2024-10-26 04:22:01 +0530

    7th search and filter inital part

--- src/pages/Pokemon/index.tsx
+++ src/pages/Pokemon/index.tsx
@@ -1,8 +1,8 @@
 import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import { useRef, useCallback, useState } from 'react';
 import axios, { AxiosResponse } from 'axios';
-import { limit } from '@/constants/index.js';
-import { Link, Outlet, useLocation } from 'react-router-dom';
+import { limit, maxPokemon } from '@/constants/index.js';
+import { Link, Outlet, useLocation, useSearchParams } from 'react-router-dom';
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 import { Trash2, X } from 'lucide-react';
@@ -11,13 +11,45 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@
 import { Input } from '@/components/ui/input';
 
 const fetchPokemon = async (pageParam = 0, queryKey: string[]): Promise<any> => {
-  let response: AxiosResponse;
+  let response: AxiosResponse
+  let pokemonList: any[] = [];
+  let check:boolean = true;
 
   try {
-    response = await axios.get(`https://pokeapi.co/api/v2/${queryKey[0]}?limit=${limit}&offset=${pageParam}`);
+
+    if (queryKey[2].length > 0) { 
+      // runs when filter ✅ is applied
+      response = await axios.get(`https://pokeapi.co/api/v2/type/${queryKey[2]}`); 
+      pokemonList = response.data.pokemon.map((item: any) => {
+        return { ...item.pokemon };
+      });
+      check = response.data.next != null
+    }
+
+    if(queryKey[1].length > 0) {
+      // runs when search ✅ is applied
+      if(pokemonList.length !== 0) {
+        // runs when search ✅ + filter✅ is applied
+        pokemonList = pokemonList.filter(pokemon => pokemon.name.includes(queryKey[1]))
+      } else {
+        // runs when search ✅  is applied but not filter ❌
+        response = await axios.get(`https://pokeapi.co/api/v2/${queryKey[0]}?limit=${maxPokemon}`);
+        pokemonList = response.data.results;
+        pokemonList = pokemonList.filter(pokemon => pokemon.name.includes(queryKey[1]))
+        check = response.data.next != null
+      }
+    }
+
+    if (queryKey[1].length === 0 && queryKey[2].length === 0) {
+      response = await axios.get(`https://pokeapi.co/api/v2/${queryKey[0]}?limit=${limit}&offset=${pageParam}`);
+      pokemonList = response.data.results;
+      check = response.data.next != null
+    }
+
+    
 
     const resultsWithDetails = await Promise.all(
-      response.data.results.map(async (pokemon: any) => {
+      pokemonList.map(async (pokemon: any) => {
         const data = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
         return { ...pokemon, details: data.data };
       }),
@@ -25,8 +57,8 @@ const fetchPokemon = async (pageParam = 0, queryKey: string[]): Promise<any> => 
 
     return {
       results: resultsWithDetails,
-      nextOffset: pageParam + limit,
-      hasNextPage: response.data.next !== null,
+      nextOffset: queryKey[1].length > 0 || queryKey[2].length > 0 ? 0 : pageParam + limit,
+      hasNextPage:( queryKey[1].length > 0 || queryKey[2].length > 0 ) ? false : check,
     };
   } catch (error: any) {
     console.log(error.message);
@@ -40,6 +72,10 @@ const fetchPokemon = async (pageParam = 0, queryKey: string[]): Promise<any> => 
 
 const Pokemon = () => {
   const location = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const [searchQuery, setSearchQuery] = useState(searchParams.get('q') || '');
+  const [category, setCategory] = useState(searchParams.get('category') || '');
 
   const {
     fetchNextPage,
@@ -48,11 +84,12 @@ const Pokemon = () => {
     hasPreviousPage,
     isFetchingNextPage,
     isFetchingPreviousPage,
+    isFetching,
     promise,
     ...result
   } = useInfiniteQuery({
-    queryKey: ['pokemon'],
-    queryFn: async (lastPage) => await fetchPokemon(lastPage.pageParam, ['pokemon']),
+    queryKey: ['pokemon', searchQuery, category],
+    queryFn: async (lastPage) => await fetchPokemon(lastPage.pageParam, ['pokemon', searchQuery, category]),
     initialPageParam: 0,
     getNextPageParam: (lastPage) => (lastPage.hasNextPage ? lastPage.nextOffset : undefined),
     staleTime: Infinity,
@@ -98,18 +135,34 @@ const Pokemon = () => {
       <div className="grid grid-cols-1 md:grid-cols-3">
         <div
           className={cn(
-            'w-full h-full flex flex-wrap gap-x-7 gap-y-24 px-6 items-center  justify-center md:justify-end md:col-span-2 pt-20',
+            'w-full h-full flex flex-wrap gap-x-7 gap-y-24 px-6 items-center  justify-center md:col-span-2 py-10',
             mobileViewPageOpen ? 'hidden' : '',
           )}
         >
-          <div className="w-full  h-9 md:w-[584px] lg:w-[776px] ml-10 lg:ml-0 flex gap-5 justify-center items-center">
-            <Input placeholder="search" />
+          <div className="w-full  h-9 md:w-[584px] lg:w-[776px] ml-10 lg:ml-0 flex gap-5 justify-center items-center self-start">
+            <Input
+              placeholder="search"
+              value={searchQuery}
+              onChange={(e) => {
+                setSearchQuery(e.target.value);
+                setSearchParams((prev) => {
+                  prev.set('q', e.target.value);
+                  return prev;
+                });
+              }}
+            />
 
             <Select
-            // onValueChange={(e) => {
-            //   handleChange(e);
-            // }}
-            // defaultValue=""
+              onValueChange={(e) => {
+                setCategory(e);
+                setSearchParams((prev) => {
+                  prev.set('category', e);
+                  prev.delete('q');
+                  return prev;
+                });
+                fetchNextPage();
+              }}
+              value={category}
             >
               <SelectTrigger className="w-[180px]">
                 <SelectValue placeholder="Type" />
@@ -123,16 +176,27 @@ const Pokemon = () => {
                   ))}
               </SelectContent>
             </Select>
-            <Button onClick={() => {}} className="p-2">
+            <Button
+              onClick={() => {
+                setCategory('');
+                setSearchQuery('');
+                setSearchParams((prev) => {
+                  prev.delete('q');
+                  prev.delete('category');
+                  return prev;
+                });
+              }}
+              className="p-2"
+            >
               <Trash2 size={18} strokeWidth={2} />
             </Button>
           </div>
 
-          {result.data?.pages.map((page: any, pageIndex) =>
+          {result.data?.pages.map((page: any) =>
             page.results.map((pokemon: any, index: number) => {
               const isLastPokemon = page.results.length === index + 1;
-              const tempIdx = pageIndex * limit + index + 1;
-              const imageNum = tempIdx <= 1025 ? tempIdx : 10000 + (pageIndex - 41) * limit + index + 1;
+              const size = pokemon.url.length - 1;
+              const imageNum = pokemon.url.substring(34, size);
               return (
                 <Link
                   key={pokemon.name}
@@ -146,7 +210,7 @@ const Pokemon = () => {
                     className="render-pixel absolute h-20 w-20 scale-150 -translate-y-20"
                     alt={pokemon.name}
                   />
-                  <h2 className="text-sm text-primary/60 font-bold pt-2 text-center truncate">N° {tempIdx}</h2>
+                  <h2 className="text-sm text-primary/60 font-bold pt-2 text-center truncate">N° {imageNum}</h2>
                   <h1 className="text-xl text-secondary-foreground tracking-tight font-semibold leading-5 truncate w-full text-center px-5">
                     {pokemon.name}
                   </h1>
@@ -161,7 +225,7 @@ const Pokemon = () => {
               );
             }),
           )}
-          {isFetchingNextPage && <p className="text-center content-center w-full">Loading...</p>}
+          {(isFetchingNextPage || isFetching) && <p className="text-center content-center w-full">Loading...</p>}
         </div>
         <div className={cn('md:block w-full', !mobileViewPageOpen ? 'hidden' : '')}>
           <div className="sticky top-16 right-0">
